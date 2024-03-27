aec2f2d5b3e39c2b421918e4c7721e2c7bf9e73e
lalmendra <lucascontatoprincipal@gmail.com>
2024-03-27 17:21:56 -0300

    update

---
+++ db_conn_model.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn){
+    die("Connection failed " . mysqli_connect_error());
+}
+echo "Connected Succesfully";
+
+mysqli_close(($conn));
+?>
